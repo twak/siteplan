c8ef80c90e2c47f2210e702ccd482297c97ef2df
twak <twakelly@gmail.com>
2017-02-15 10:51:20 +0000

    ?!

--- src/org/twak/siteplan/anim/SaturdayPA.java
+++ src/org/twak/siteplan/anim/SaturdayPA.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.twak.straightskeleton.ui.Bar;
-import org.twak.utils.Cluster1D;
+import org.twak.utils.DumbCluster1D;
 import org.twak.utils.Line;
 import org.twak.utils.LoopL;
 
